1b9094c1ccd722103c6a482a0e8db729f274c7f6
Megan <megango@Megans-MacBook-Pro.local>
2025-07-01 21:00:45 +0800

    first commit

--- set_3.py
+++ set_3.py
@@ -46,28 +46,3 @@ def eta(first_stop, second_stop, route_map):
                 now=end
                 break
     return total_time
-
-legs1 = {
-     ("upd","admu"):{
-         "travel_time_mins":10
-     },
-     ("admu","dlsu"):{
-         "travel_time_mins":35
-     },
-     ("dlsu","upd"):{
-         "travel_time_mins":55
-     }
-}
-
-legs2 = {
-    ('a1', 'a2'): {
-        'travel_time_mins': 10
-    },
-    ('a2', 'b1'): {
-        'travel_time_mins': 10230
-    },
-    ('b1', 'a1'): {
-        'travel_time_mins': 1
-    }
-}
-print(eta("b1","a1",legs2))
